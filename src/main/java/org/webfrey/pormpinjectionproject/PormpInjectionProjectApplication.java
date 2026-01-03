3e7b0a8203626dc6a3c69622539f7cc0cb775fc9
k9ne257 <aafre101@uni-duesseldorf.de>
2026-01-03 20:27:09 +0100

    migrated files

---
+++ src/main/java/org/webfrey/pormpinjectionproject/PormpInjectionProjectApplication.java
@@ -0,0 +1,13 @@
+package org.webfrey.pormpinjectionproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PormpInjectionProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PormpInjectionProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/webfrey/pormpinjectionproject/PormpInjectionProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package org.webfrey.pormpinjectionproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PormpInjectionProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
